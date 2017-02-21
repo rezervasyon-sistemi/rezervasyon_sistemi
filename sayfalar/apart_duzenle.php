43d7f84a9272a40b6f6cd179e2085e0faec59de0
Ömer Yücel <omeryucel94@gmail.com>
2017-02-21 20:37:59 +0200

    Update apart_duzenle.php

--- sayfalar/apart_duzenle.php
+++ sayfalar/apart_duzenle.php
@@ -1,3 +1,42 @@
+<?php
+
+	include "../fonksiyonlar/apart_islemleri.php";
+	header('Content-Type: text/html; charset=utf-8');
+	
+	$divResult='';
+	$apartBilgileri=array();
+	$apartBilgileri[0]=0;
+	$apartBilgileri[1]="";
+	$apartBilgileri[2]="";
+	$apartNo='';
+	
+	if( isset($_GET['apart_no']))
+	{
+		$apartNo=$_GET['apart_no'];
+		$apartID=apartIDGetir($apartNo);
+		$apartBilgileri=apartBilgileriGetir($apartID);
+	}
+	
+	if( isset($_POST['yeni_apart_adi']) && isset($_POST['yeni_oda_tipi']) )
+	{
+		$apartID=$_POST['apart_id'];
+		$yeniApartAdi=$_POST['yeni_apart_adi'];
+		$yeniOdaTipi=$_POST['yeni_oda_tipi'];
+		
+		if ( apartDuzenle($apartID,$yeniApartAdi,$yeniOdaTipi) ) 
+		{
+			$divResult='<div class="alert alert-success alert-dismissible fade in"><strong>Apart Başarıyla Güncellenmiştir!</strong></div>';
+		} 
+		else 
+		{
+			$divResult='<div class="alert alert-danger alert-dismissible fade in"><strong>Apart Güncellenememiştir!</strong></div>';
+		} 
+	}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -35,7 +74,6 @@
 		<div class="col-md-3 left_col menu_fixed">
           <div class="left_col scroll-view">
              
-
             <!-- sidebar menu -->
 			<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
@@ -63,15 +101,21 @@
                     </ul>
                   </li>
 				  
-                  <li><a href="rezervasyon_satis.php"><i class="fa fa-money"></i> Rezervasyon Satış </a></li>
+                  <li><a href="rezervasyon_satis.php"><i class="fa fa-credit-card"></i> Rezervasyon Satış </a></li>
                    
 				  <li><a href="apart_durumlari.php"><i class="fa fa-calendar"></i> Apart Durumları </a></li>
 				  
                   <li><a href="apart_etkinlikleri.php"><i class="fa fa-exchange"></i>Apart Etkinlikleri </a></li>
                                         
-				  <li><a href="istatistikler.php"><i class="fa fa-bar-chart-o"></i> İstatistikler</a></li>
-				  
-				  <li><a href="musteri_bilgileri.php"><i class="fa fa-info-circle"></i> Müşteri Bilgileri </a></li>
+				   <li><a href="musteri_bilgileri.php"><i class="fa fa-info-circle"></i> Müşteri Bilgileri </a></li>
+				
+				  <li>
+					<a><i class="fa fa-bar-chart-o"></i> İstatistikler <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="apart_istatistikleri.php">Apart İstatistikleri</a></li>
+                      <li><a href="donem_istatistikleri.php">Dönem İstatistikleri</a></li>
+                    </ul>
+                  </li>
                 </ul>
                  
               </div>
@@ -93,11 +137,7 @@
 
         <!-- page content -->
         <div class="right_col" role="main">
-          
           <!-- /top tiles -->
-
-
-        
 			<div class="row">
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
@@ -106,27 +146,39 @@
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
-                    <br />
-                    <form align="center" id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+                    <br/>
+                    <form class="form-horizontal form-label-left" align="center" id="form1" name="form1" method="GET" data-parsley-validate >
 
-					<div class="form-group">
-                        <label class="control-label col-md-5 col-sm-6 col-xs-12">Apartı Seçiniz</label>
-                        <div class="col-md-2 col-sm-6 col-xs-9">
-                          <select class="select2_single form-control" tabindex="-1">
-                            <option value="AK">Apart-1</option>
-                            <option value="HI">Apart-2</option>
-                            <option value="CA">Apart-3</option>
-                          </select>
-                        </div>
-                      </div>
+						<div class="form-group">
+							<label class="control-label col-md-5 col-sm-6 col-xs-12">Apart Seçiniz</label>
+							<div class="col-md-2 col-sm-6 col-xs-9">
+								<?php
+								
+									$apartNoListesi=array();
+									$apartNoListesi=kayitliApartNoListesi();
+									$uzunluk=count($apartNoListesi);
+									if($uzunluk == 0) 
+									{
+										$divResult='<div class="alert alert-warning alert-dismissible fade in"><strong>Sistemde Apart Kaydı Bulunmamaktadır!</strong></div>';
+									} 
+								?>
+								<select class="select2_single form-control" name="apart_no" id="apart_no" tabindex="-1">
+									<?php for($i=0;$i<$uzunluk;$i++){  ?> 
+										
+										<option <?php if ($apartNoListesi[$i]==$apartNo) echo 'selected'; ?> value="<?php echo $apartNoListesi[$i];?>" ><?php echo $apartNoListesi[$i]; ?></option>
+									
+									<?php } ?>
+								</select>
+							</div>
+						</div>
 					   
-                      <div class="ln_solid"></div>
-                      <div class="form-group">
-                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-							<button type="submit" class="btn btn-success">Getir</button>
-							<button type="button" class="btn btn-danger" onclick="window.location.href='../index.php' ">İptal</button>
-                        </div>
-                      </div>
+						<div class="ln_solid"></div>
+                    
+						<div class="form-group">
+							<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+								<button type="submit" class="btn btn-success">Getir</button>
+							</div>
+						</div>
 
                     </form>
                   </div>
@@ -135,50 +187,51 @@
 				
 				   <div class="x_panel">
                   <div class="x_title">
-                    <h5>Düzenlenecek Aparta Ait Bilgileri Giriniz!</h5>
+                    <h5>Aparta Ait Bilgileri Güncelleyiniz!</h5>
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
-                    <br />
-                    <form align="center" id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
-
-					<div class="form-group">
-                        <label class="control-label col-md-4 col-sm-3 col-xs-3" for="first-name">Apart Adı <span class="required"></span></label>
-                        <div class="col-md-4 col-sm-5 col-xs-12">
-							<input type="text" id="first-name" required="required" placeholder="Apart Adı" class="form-control col-md-7 col-xs-12"></input>
-                        </div>
-                      </div>
-					 
-					<div class="form-group">
-                        <label class="control-label col-md-4 col-sm-5 col-xs-12" for="first-name">Oda Tipi <span class="required"></span>
-                        </label>
-                        <div class="col-md-4 col-sm-5 col-xs-12">
-                          <input type="text" id="first-name" required="required" placeholder="Oda Tipi" class="form-control col-md-7 col-xs-12"></input>
-                        </div>
-                      </div>
-					
-                      <div class="ln_solid"></div>
-                      <div class="form-group">
-                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-							<button type="submit" class="btn btn-success">Düzenle</button>
-							<button type="button" class="btn btn-danger" onclick="window.location.href='../index.php' ">İptal</button>
-                        </div>
-                      </div>
+                    <br/>
+                    <form class="form-horizontal form-label-left" align="center" id="form2" name="form2" method="POST" onsubmit="return confirm('Apart Düzenleme İşlemini Onaylıyor Musunuz?')" data-parsley-validate>
+						
+						<input type="hidden" id="apart_id" name="apart_id" value="<?php echo "$apartBilgileri[0]" ?>" ></input>
+							
+						<div class="form-group">
+							<label class="control-label col-md-4 col-sm-3 col-xs-3" for="apart-adi">Apart Adı <span class="required"></span></label>
+							<div class="col-md-4 col-sm-5 col-xs-12">
+								<input type="text" id="yeni_apart_adi" name="yeni_apart_adi" value="<?php echo "$apartBilgileri[1]" ?>" required="required" class="form-control col-md-7 col-xs-12"></input>
+								<span class="fa fa-info form-control-feedback right" aria-hidden="true"></span>
+							</div>
+						</div>
+						 
+						<div class="form-group">
+							<label class="control-label col-md-4 col-sm-5 col-xs-12" for="oda-tipi">Oda Tipi <span class="required"></span></label>	
+							<div class="col-md-4 col-sm-5 col-xs-12">
+								<input type="text" id="yeni_oda_tipi" name="yeni_oda_tipi" value="<?php echo "$apartBilgileri[2]" ?>" required="required" class="form-control col-md-7 col-xs-12"></input>
+								<span class="fa fa-info form-control-feedback right" aria-hidden="true"></span>
+							</div>
+						</div>
+						
+						<div class="ln_solid"></div>
+						
+						<div class="form-group">
+							<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+								<button type="submit" class="btn btn-success">Düzenle</button>
+								<button type="button" class="btn btn-danger" onclick="window.location.href='../index.php' ">İptal</button>
+							</div>
+						</div>
 
                     </form>
                   </div>
 				   
                 </div>
-				
+				<div>
+					<?php echo $divResult; ?>   
+                </div>
               </div>
-            </div>
-		
-           
+            </div> 
         </div>
-		
         <!-- /page content -->
-
-         
       </div>
     </div>
 	
